f087786b1afc3f0961df03233456f816c30ed771
Evan D. Hoffman <evandhoffman@gmail.com>
2017-12-06 18:48:18 -0600

    Added templates

--- src/delete.py
+++ src/delete.py
@@ -1,73 +1,104 @@
-#!/usr/bin/env python
+# coding: utf-8
 
-# http://www.mathewinkson.com/2015/03/delete-old-tweets-selectively-using-python-and-tweepy
-import tweepy
-from datetime import datetime, timedelta
- 
-# options
-test_mode = False
-verbose = False
-delete_tweets = True
-delete_favs = True
-days_to_keep = 7
- 
-# auth and api
-consumer_key = 'XXXXXXXX'
-consumer_secret = 'XXXXXXXX'
-access_token = 'XXXXXXXX'
-access_token_secret = 'XXXXXXXX'
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
- 
-# set cutoff date, use utc to match twitter
-cutoff_date = datetime.utcnow() - timedelta(days=days_to_keep)
- 
-# delete old tweets
-if delete_tweets:
-    # get all timeline tweets
-    print "Retrieving timeline tweets"
-    timeline = tweepy.Cursor(api.user_timeline).items()
-    deletion_count = 0
-    ignored_count = 0
- 
-    for tweet in timeline:
-        # where tweets are not in save list and older than cutoff date
-        if tweet.id not in tweets_to_save and tweet.created_at < cutoff_date:
-            if verbose:
-                print "Deleting %d: [%s] %s" % (tweet.id, tweet.created_at, tweet.text)
-            if not test_mode:
-                api.destroy_status(tweet.id)
-             
-            deletion_count += 1
-            print "Deleted Tweet #%d, from %s, text: '%s'"
+from flask import Flask
+from flask import g, session, request, url_for, flash
+from flask import redirect, render_template
+from flask_oauthlib.client import OAuth
+import json
+
+# Tokens are generated at https://apps.twitter.com/
+with open('config.json') as cf:
+    config = json.load(cf)
+
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'development'
+
+oauth = OAuth(app)
+
+
+twitter = oauth.remote_app(
+    'twitter',
+    consumer_key=config['consumer_key'],
+    consumer_secret=config['consumer_secret'],
+    base_url='https://api.twitter.com/1.1/',
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    authorize_url='https://api.twitter.com/oauth/authorize'
+)
+
+
+@twitter.tokengetter
+def get_twitter_token():
+    if 'twitter_oauth' in session:
+        resp = session['twitter_oauth']
+        return resp['oauth_token'], resp['oauth_token_secret']
+
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'twitter_oauth' in session:
+        g.user = session['twitter_oauth']
+
+
+@app.route('/')
+def index():
+    tweets = None
+    if g.user is not None:
+        resp = twitter.request('statuses/home_timeline.json')
+        if resp.status == 200:
+            tweets = resp.data
         else:
-            ignored_count += 1
- 
-    print "Deleted %d tweets, ignored %d" % (deletion_count, ignored_count)
-else:
-    print "Not deleting tweets"
-     
-# unfavor old favorites
-if delete_favs:
-    # get all favorites
-    print "Retrieving favorite tweets"
-    favorites = tweepy.Cursor(api.favorites).items()
-    unfav_count = 0
-    kept_count = 0
- 
-    for tweet in favorites:
-        # where tweets are not in save list and older than cutoff date
-        if tweet.id not in favs_to_save and tweet.created_at < cutoff_date:
-            if verbose:
-                print "Unfavoring %d: [%s] %s" % (tweet.id, tweet.created_at, tweet.text)
-            if not test_mode:
-                api.destroy_favorite(tweet.id)
-             
-            unfav_count += 1
-        else:
-            kept_count += 1
- 
-    print "Unfavored %d tweets, ignored %d" % (unfav_count, kept_count)
-else:
-    print "Not unfavoring tweets"
+            flash('Unable to load tweets from Twitter.')
+    return render_template('index.html', tweets=tweets)
+
+
+@app.route('/tweet', methods=['POST'])
+def tweet():
+    if g.user is None:
+        return redirect(url_for('login', next=request.url))
+    status = request.form['tweet']
+    if not status:
+        return redirect(url_for('index'))
+    resp = twitter.post('statuses/update.json', data={
+        'status': status
+    })
+
+    if resp.status == 403:
+        flash("Error: #%d, %s " % (
+            resp.data.get('errors')[0].get('code'),
+            resp.data.get('errors')[0].get('message'))
+        )
+    elif resp.status == 401:
+        flash('Authorization error with Twitter.')
+    else:
+        flash('Successfully tweeted your tweet (ID: #%s)' % resp.data['id'])
+    return redirect(url_for('index'))
+
+
+@app.route('/login')
+def login():
+    callback_url = url_for('oauthorized', next=request.args.get('next'))
+    return twitter.authorize(callback=callback_url or request.referrer or None)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('twitter_oauth', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/oauthorized')
+def oauthorized():
+    resp = twitter.authorized_response()
+    if resp is None:
+        flash('You denied the request to sign in.')
+    else:
+        session['twitter_oauth'] = resp
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run()
