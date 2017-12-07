357eae403ff88c5dd03f052f357ce3c520fb1adc
Evan D. Hoffman <evandhoffman@gmail.com>
2017-12-07 12:05:13 -0600

    more logging

--- src/delete.py
+++ src/delete.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask import g, session, request, url_for, flash
 from flask import redirect, render_template
 from flask_oauthlib.client import OAuth
-import json, datetime
+import json, datetime, time
 
 # Tokens are generated at https://apps.twitter.com/
 with open('config.json') as cf:
@@ -17,7 +17,7 @@ app.secret_key = 'development'
 
 oauth = OAuth(app)
 
-max_tweets_to_process = 1000
+max_tweets_to_process = 5000
 
 
 twitter = oauth.remote_app(
@@ -49,22 +49,34 @@ def before_request():
 def index():
     tweets = None
     if g.user is not None:
-        processed_count = 0
+        deleted_count = 0
+        evaluated_count = 0
         max_id = 0 
-        while (processed_count < max_tweets_to_process ):
-            if (max_id > 1):
+        batch = 0
+        while (deleted_count < max_tweets_to_process ):
+            batch += 1
+            print ("Currently working on batch # %d" % batch)
+            if (max_id > 0):
                 req = "statuses/user_timeline.json?count=200&max_id=%d" % max_id
             else:
                 req = "statuses/user_timeline.json?count=200"
+            print "Making request: %s" % req
             resp = twitter.request(req)
             if resp.status == 200:
                 tweets = resp.data
+                print ("Got %d more tweets from Twitter" % len(tweets))
                 for tweet in tweets:
+                    evaluated_count += 1
                     if (max_id == 0 or tweet['id'] < max_id):
-                        max_id = tweet['id']
-                tweets = delete_tweets(tweets, 1)
-                processed_count += len(tweets)
-                print ("processed %d tweets so far, max_id is now %d" % (processed_count, max_id))
+                        max_id = tweet['id']-1
+                tweets_to_delete = delete_tweets(tweets, 365)
+                for tweet in tweets_to_delete:
+                    deleted_count += 1
+                    print ("Gonna delete Tweet #%d with timestamp %s" % (tweet['id'], tweet['created_at']))
+                    twitter.post("statuses/destroy/%d" % tweet['id'])
+                    print ("DELETED POST %d" % tweet['id'])
+                print ("Looked at %d tweets so far, deleted %d, max_id is now %d" % (evaluated_count, deleted_count, max_id))
+                time.sleep(1)
             else:
                 flash('Unable to load tweets from Twitter.')
                 break
@@ -80,6 +92,8 @@ def delete_tweets(tweets, threshold_days=365 ):
         age_days = divmod(age.total_seconds(), 86400)[0]
         if (age_days > threshold_days):
             filtered_tweets.append(tweet)
+        else: 
+            print ("Skipping Tweet #%d with timestamp %s" % (tweet['id'], tweet['created_at']))
     return filtered_tweets
 
 
